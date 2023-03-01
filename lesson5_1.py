5d4f4ad63dfd7bf279cde53fc774c858a3c9b0fc
Takao Hattori <nogizakapython@outlook.jp>
2023-03-01 13:36:10 +0900

    Modefy20230301Afternoon

---
+++ lesson5_1.py
@@ -0,0 +1,12 @@
+# 新規作成 2023/3/1
+# 数値の表現
+# print(10000)
+# print(10000 * 1.1)
+# print(10000 * 1.1 * 1.1)
+
+money = 10000
+rate = 1.1
+
+print(money)
+print(money * rate)
+print(money * rate * rate)
