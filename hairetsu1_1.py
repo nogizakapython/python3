9c3cbff135437f8f9dac61560d1ad386edebdc6c
Takao Hattori <nogizakapython@outlook.jp>
2023-03-06 13:49:50 +0900

    Modefy20230306afternoon

---
+++ hairetsu1_1.py
@@ -0,0 +1,11 @@
+# 配列の要素を取り出す
+# 配列の要素を足す
+
+scores = [10,40,20,30,50]
+sum = 0
+
+for i in scores:
+  sum += i
+  print(i)
+
+print(sum)
---
+++ hairetsu2_1.py
@@ -0,0 +1,14 @@
+# 配列の要素を表示する
+# 新規作成 2023/3/6
+
+scores = [10,40,20,30,50]
+
+print(scores)
+print(scores[0])
+print(scores[1])
+scores[2] = 100
+print(scores[2])
+
+num = len(scores)
+# 配列の要素数を表示する
+print(num)
