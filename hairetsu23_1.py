958a4954994566b40665804362f83fb5b1fba603
Takao Hattori <nogizakapython@outlook.jp>
2023-03-10 14:00:45 +0900

    Modefy20230310Afternoon

---
+++ hairetsu23_1.py
@@ -0,0 +1,23 @@
+# 新規作成 2023/3/10
+# 辞書のソート処理
+
+scores = [
+  {"name": "Taro", "math": 70, "english": 82},
+  {"name": "Jiro", "math": 67, "english": 61},
+  {"name": "Saburo", "math": 81, "english": 58},
+  {"name": "Siro", "math": 47, "english": 70},
+]
+
+print("Name     Math     English")
+print("-------- -------- --------")
+
+# def compare(score):
+#     return score["math"]
+
+#lambda score: score["math"]
+
+scores.sort(key=lambda score:score["math"],reverse=True)
+for score in scores:
+    for value in score.values():
+        print(f"{value:8} ", end="")
+    print()
