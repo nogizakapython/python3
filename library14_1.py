2f49e92a6010b2d1b51dabc7b1d8bbc414946bc0
Takao Hattori <nogizakapython@outlook.jp>
2023-03-14 12:09:48 +0900

    Modefy20230314noon

---
+++ library14_1.py
@@ -0,0 +1,15 @@
+# 配列の出力を整える処理
+# 新規作成 2023/3/14
+
+# pprintライブラリを読み込む
+# import pprint
+from pprint import pprint
+
+scores = [
+  {"name": "Taro", "math": 70, "english": 82},
+  {"name": "Jiro", "math": 67, "english": 61},
+  {"name": "Saburo", "math": 81, "english": 58},
+]
+
+# print(scores)
+pprint(scores)
