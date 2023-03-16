ce3299e2559892e9d79ed4460159d35b300e1273
Takao Hattori <nogizakapython@outlook.jp>
2023-03-16 14:24:55 +0900

    Modefy20230316afternoon

---
+++ class13_1.py
@@ -0,0 +1,19 @@
+# クラスのオーバーロード
+
+class Human():
+    def __init__(self, name="Jonson", age=20):
+        self.name = name
+        self._age = age
+    def show(self):
+        print(f"私の名前は{self.name}です。年齢は{self._age}です。")
+    def likes(self):
+        self._age += 1
+jonson = Human()
+jonson.show()
+man = Human("taro", 21)
+man.show()
+woman = Human("mariko")
+woman.show()
+mika = Human("mika",30)
+mika.likes()
+mika.show()
