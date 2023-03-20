c46a68c2476f53733b6338053a708cd5a3297b4e
Takao Hattori <nogizakapython@outlook.jp>
2023-03-20 13:56:01 +0900

    Modefy20230320Afternoon

---
+++ anaconda2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("ようこそPython3")
+
+
+# In[2]:
+
+
+print(50)
+
+
+# In[1]:
+
+
+print(len('Python'))
+
+
+# In[2]:
+
+
+num = 50
+print(num)
+num = 100
+print(num)
+
+
+# In[3]:
+
+
+print('Python' + 'を始めよう')
+
+
+# In[4]:
+
+
+a = 2
+print(f"aの値は{a}です")
+
+
+# In[5]:
+
+
+print(a)
+
+
+# In[ ]:
+
+
+
+
