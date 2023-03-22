971206c3b04019952c7ec5b6b50908725072cbe1
takao hattori <nogizakapython@outlook.jp>
2023-03-22 13:23:32 +0900

    Modefy20230322Afternoon

---
+++ progate24_1.py
@@ -0,0 +1,17 @@
+# 新規作成　2023/3/22
+# クラスを分割し、MenuItemを表示する
+
+from progate23_2 import MenuItem
+
+menu_item1 = MenuItem('サンドイッチ', 500)
+menu_item2 = MenuItem('チョコケーキ', 400)
+menu_item3 = MenuItem('コーヒー', 300)
+menu_item4 = MenuItem('オレンジジュース', 200)
+
+# 指定されたリストを変数 menu_items に代入してください
+menu_items = [ menu_item1, menu_item2,menu_item3,menu_item4]
+
+# for 文を作成してください
+for item in menu_items:
+  print(item.info())
+  
