172b72beb62aae43355525cfeb46591e1333f6d1
takao hattori <nogizakapython@outlook.jp>
2023-03-22 11:35:52 +0900

    Modefy20230322Morning

--- progate18_2.py
+++ progate18_2.py
@@ -1,4 +1,6 @@
 # MenuItem クラスを定義してください
+# 新規作成 2023/3/22
+
 class MenuItem:
   def call(name):
     print(name + "さん、こんにちは")
---
+++ progate19_1.py
@@ -0,0 +1,29 @@
+# 新規作成 2023/3/22
+# クラス定義
+
+class MenuItem:
+    pass
+
+
+menu_item1 = MenuItem()
+
+menu_item1.name = 'サンドイッチ'
+print(menu_item1.name)
+
+menu_item1.price = 500
+print(menu_item1.price)
+
+# MenuItem クラスのインスタンスを生成してください
+menu_item2 = MenuItem()
+
+# menu_item2 の name に「 チョコケーキ 」を代入してください
+menu_item2.name = 'チョコケーキ'
+
+# menu_item2 の name を出力してください
+print(menu_item2.name)
+
+# menu_item2 の price に 400 を代入してください
+menu_item2.price = 400
+
+# menu_item2 の price を出力してください
+print(menu_item2.price)
