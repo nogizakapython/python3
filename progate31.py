2f4bd25639489c90e6d391a0d7dc7f5cc7af5d0c
Takao Hattori <nogizakapython@outlook.jp>
2023-03-24 11:09:02 +0900

    Modefy20230324Morning

---
+++ progate31.py
@@ -0,0 +1,14 @@
+# 新規作成 2023/3/24
+# 継承したクラスのオーバーライド
+
+from progate31_food import Food
+from progate31_drink import Drink
+
+food1 = Food('サンドイッチ', 500,330)
+# food1 に対して info メソッドを呼び出して戻り値を出力してください
+print(food1.info())
+
+drink1 = Drink('コーヒー',300)
+drink1.amount = 300
+
+print(drink1.info())
---
+++ progate31_food.py
@@ -0,0 +1,17 @@
+# Food クラス
+# 新規作成 2023/3/24
+
+from progate31menu_item import MenuItem
+
+class Food(MenuItem):
+    def __init__(self, name, price, calorie):
+        # super() を用いて、親クラスの __init__() を呼び出してください
+        super().__init__(name,price)
+        # 差分のカロリー部分を追加
+        self.calorie = calorie
+
+    def info(self):
+        return self.name + ': ¥' + str(self.price) + ' (' + str(self.calorie) + 'kcal)'
+
+    def calorie_info(self):
+        print(str(self.calorie) + 'kcalです')
