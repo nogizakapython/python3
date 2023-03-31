67750acec46212e84230c33da1670d97f943a2ff
Takao Hattori <nogizakapython@outlook.jp>
2023-03-31 11:13:03 +0900

    Modefy20230331Morning

---
+++ jupyter3.py
@@ -0,0 +1,17 @@
+# 新規作成 2023/3/31
+# jupyter notebookでのソース
+
+#配列の定義
+cities = ['東京','パリ','ロンドン','北京']
+
+# 配列の要素を全て取り出す
+for city in cities:
+    print(city)
+
+# 配列の追加
+cities.append("ミラノ")
+
+# 配列の要素を全て取り出す
+for city in cities:
+    print(city)
+
