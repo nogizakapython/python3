ce5254653019d20ca1df01e2a5989f731fa22327
Takao Hattori <nogizakapython@outlook.jp>
2023-05-09 07:03:17 +0900

    Modefy20230509Morning

---
+++ test10.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+data = input()
+array1 = data.split(' ')
+for i in range(1,len(array1)):
+    print(array1[i])
---
+++ test9.py
@@ -0,0 +1,8 @@
+data = input()
+array1 = data.split(' ')
+A = int(array1[0])
+B = int(array1[1])
+if A == 0 or B == 0:
+  print(0)
+else:
+  print(1)
