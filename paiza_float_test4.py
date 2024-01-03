46d5fb790bba0f8d5458295ddfcf2cc6e10ad327
Takao Hattori <nogizakapython@outlook.jp>
2024-01-04 08:25:19 +0900

    Modefy20240104Mon

---
+++ paiza_float_test4.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+while True:
+  try:
+    num = int(input("データ数を正の整数で入力してください"))
+    if num < 1:
+      raise ValueError
+    else:
+      break
+  except ValueError as e:
+      print("正の整数以外は入力しないでください")
+
+for i in range(0,num):
+    data1 = input()
+    array1 = data1.split(' ')
+    N = float(array1[0])
+    M = int(array1[1])
+    ans = "{:.{}f}".format(N,M)
+    print(ans)
