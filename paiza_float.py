502b0e67a70a9bca01a46fdc0ff52acd29413e59
Takao Hattori <nogizakapython@outlook.jp>
2024-01-04 07:32:19 +0900

    Modefy20240104morning

---
+++ paiza_float.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+input_line = float(input("小数を入力してください\n"))
+print("{:.3f}".format(input_line))
---
+++ paiza_float2.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+num = float(input("小数を入力してください\n"))
+ans = "{:.4f}".format(num)
+print(ans)
