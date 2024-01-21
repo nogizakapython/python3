43ca65f098d039aa39914938c8cf5847b0ba4258
Takao Hattori <nogizakapython@outlook.jp>
2024-01-22 06:23:49 +0900

    20240122

---
+++ paiza_hairetsu1-1.py
@@ -0,0 +1,3 @@
+# Let's チャレンジ！！
+array1 = [[1,2,3,4,5,6],[8,1,3,3,1,8]]
+print(len(array1) * len(array1[0]))
---
+++ paiza_hairetsu1.py
@@ -0,0 +1,3 @@
+# Let's チャレンジ！！
+array1 = [[1,2,3,4,5,6],[8,1,3,3,1,8]]
+print(sum(len(i) for i in array1))
