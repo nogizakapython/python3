ad6ce85139775a60059bd1801a140c7991adb219
Takao Hattori <nogizakapython@outlook.jp>
2024-02-09 15:38:03 +0900

    Modefy20240209

--- replace2.py
+++ replace2.py
@@ -9,10 +9,12 @@ import os
 IN_FILE = "gra_nogizaka.txt"
 OUT_FILE = "gra_nogizakareplace.txt"
 
-#
+# 古い置換ファイルがあれば削除
 if os.path.exists(OUT_FILE):
     os.remove(OUT_FILE)
 
+# 半角スペースファイルから１行ずつ読み込み、半角スペースをtabスペースに置換し、ファイルに
+# 出力する。
 with open(IN_FILE,mode='r',encoding='utf-8') as f:
     for line in f:
         line1 = line.replace(" ","\t")
