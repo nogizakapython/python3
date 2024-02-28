05f5b2fce741dba509c70a539f8a6f2eeed109c2
Takao Hattori <nogizakapython@outlook.jp>
2024-02-28 17:44:35 +0900

    Modefy20240228

--- bycycle_electrnic.py
+++ bycycle_electrnic.py
@@ -1,10 +1,8 @@
 from bycycle import Bycycle
 
 class ElectricBycycle(Bycycle):
-  def __init__(self, name="人"):
-    self.__name = super().__init__(name)
 
-  def electronic_ride(self):
+  def electronic_ride(self,name):
 
-    print(f"{self.__name}は電動自転車を漕ぐ")
+    print(f"{name}は電動自転車を漕ぐ")
 
