4eecbd83aee4bb6855e00ef981653822d0032133
Takao Hattori <nogizakapython@outlook.jp>
2024-03-06 12:39:55 +0900

    Modefy20240306

---
+++ paiza_hairetsu30.py
@@ -0,0 +1,6 @@
+array1 = [[1,2,3,4],[10,100,0,5],[8,1,3,8],[15,34,94,25]]
+input_line = input()
+w_array1 = input_line.split(' ')
+k = int(w_array1[0])
+l = int(w_array1[1])
+print(array1[k-1][l-1])
