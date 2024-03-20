58a253bf68293ec265c461775d00b57822c52db3
Takao Hattori <nogizakapython@outlook.jp>
2024-03-20 20:13:34 +0900

    Modefy20240320

---
+++ listtest3.py
@@ -0,0 +1,21 @@
+class Test1:
+
+  array1 = []
+
+  def input_data(self,data1):
+    self.array1.append(data1)
+    return self.array1
+
+
+def main():
+  data_count = 3
+  t1 = Test1()
+  for i in range(data_count):
+    data1 = input()
+    array_sample1 = t1.input_data(data1)
+    taple1 = tuple(array_sample1)
+
+  print(taple1)
+
+if __name__ == '__main__':
+  main()
