a5214575fb77cdec1d37ba9920c086b3e70a6721
Takao Hattori <nogizakapython@outlook.jp>
2024-04-09 17:53:42 +0900

    Modefy20240409

---
+++ otaru_2010-1.py
@@ -0,0 +1,18 @@
+# 小樽商科大2010年の入試問題
+# 2010!は2で何回割り切れるか
+
+
+def two_div(num):
+  two_div_count = 0
+  while num % 2 == 0:
+    two_div_count += 1
+    num /= 2
+  return two_div_count
+
+def main():
+  N = 2010
+  two_div_ans = sum(two_div(i) for i in range(1,N+1))
+  print(two_div_ans)
+
+if __name__ == "__main__":
+  main()
