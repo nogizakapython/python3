919d171182e05d8df771296dc7472db6cc6dc10c
Takao Hattori <nogizakapython@outlook.jp>
2024-06-04 07:24:28 +0900

    Modefy20240604

---
+++ paiza_hairetsu29.py
@@ -0,0 +1,10 @@
+a,b = map(int,input().split())
+array1 = list(map(int,input().split()))
+array2 = list(map(int,input().split()))
+array3 = []
+for i in array1:
+    array3.append(i)
+for j in array2:
+    array3.append(j)
+for k in array3:
+    print(k)
