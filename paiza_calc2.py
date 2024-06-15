d355f4428074a30185ccfddbc44d18f9f2708e5e
Takao Hattori <nogizakapython@outlook.jp>
2024-06-15 20:15:53 +0900

    Modefy20240615

--- paiza_calc2.py
+++ paiza_calc2.py
@@ -1,4 +1,12 @@
 import re
+
+def add(ans,num):
+  ans += num
+  return ans
+
+def gain(ans,num):
+  ans -= num
+  return ans
 str1 = input()
 array1 = list(map(int,re.split('[+-]',str1)))
 # print(array1)
@@ -11,18 +19,18 @@ for i in range(len1):
     if i == len1-1:
       number1 = array1[p]
       if calc1 == "+":
-        ans += number1
+        ans = add(ans,number1)
       elif calc1 == "-":
-        ans -= number1
+        ans = gain(ans,number1)
     if str2 == "+" or str2 == "-":
         number1 = array1[p]
         if p == 0:
-            ans += number1
+            ans = add(ans,number1)
         elif p > 0:
             if calc1 == "+":
-                ans += number1
+                ans = add(ans,number1)
             else:
-                ans -= number1
+                ans = gain(ans,number1)
         if str2 == "+":
             calc1 = str2
         else:
