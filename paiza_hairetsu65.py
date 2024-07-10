df49196c80ef4d4d0411955fad2c9ba2852547dc
Takao Hattori <nogizakapython@outlook.jp>
2024-07-11 06:35:54 +0900

    Modefy20240711

---
+++ paiza_hairetsu65.py
@@ -0,0 +1,11 @@
+num=int(input())
+array1 = []
+for i in range(num):
+    data = int(input())
+    array1.append(data)
+
+data1 = int(input())
+array1.append(data1)
+
+for k in array1:
+    print(k)
