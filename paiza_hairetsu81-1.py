1784198410458c55e781ebeed8a494f06f888eff
Takao Hattori <nogizakapython@outlook.jp>
2024-07-22 07:16:17 +0900

    Modefy20240722

---
+++ paiza_hairetsu81-1.py
@@ -0,0 +1,14 @@
+p = []
+ans_p = 0
+ans_num = 0
+
+for i in range(4):
+    p += input().split()
+
+for i in range(10):
+    if p[i] == "1":
+        ans_num += 1
+        ans_p = 10 - i
+
+print(ans_p)
+print(ans_num)
