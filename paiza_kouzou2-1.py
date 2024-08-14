3bd559d38b8bd6f1c11493a51d29eca8851249db
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-08-14 13:51:00 +0900

    Modefy20240814

---
+++ paiza_kouzou2-1.py
@@ -0,0 +1,21 @@
+class Student:
+    def __init__(self, name, old, birth, state):
+        self.name = name
+        self.old = old
+        self.birth = birth
+        self.state = state
+
+
+n = int(input())
+
+roster = [None] * n
+for i in range(n):
+    name, old, birth, state = input().split()
+    roster[i] = Student(name, old, birth, state)
+    
+
+k = input()
+for student in roster:
+    if student.old == k:
+        print(student.name)
+        break
