bb6c9d3ecec4a412e8e7dadd3b92ec6774707ea0
meitetsu5000 <nogizakapython@outlook.jp>
2024-08-24 19:32:27 +0900

    Modefy20240824

---
+++ paiza_query6-1.py
@@ -0,0 +1,8 @@
+N, K = map(int, input().split())
+roster = {(x, y) for x, y in [input().split() for _ in range(N)]}
+
+for _ in range(K):
+    q = input()
+    for num, ID in roster:
+        if num == q:
+            print(ID)
