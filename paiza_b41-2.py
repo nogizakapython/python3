fe31b7c2a6d708f5075e68be4f7a2bc8c63b7224
meitetsu5000 <nogizakapython@outlook.jp>
2024-09-17 07:19:45 +0900

    Modefy20240917

--- paiza_b41-2.py
+++ paiza_b41-2.py
@@ -1,11 +1,16 @@
 array1 = ["HND", "NRT", "KIX", "NGO", "NGO"]
 array2 = list(set(array1))
 count = 0
+chech = 0
 for i in range(len(array2)):
+    count = 0
     for j in range(len(array1)):
-        if array2[i] == array1[j] and i != j:
+        if array2[i] == array1[j]:
             count += 1
-if count > 0:
+    if count > 1:
+        check = 1
+
+if check > 0:
     print("true")
 else:
     print("false")
