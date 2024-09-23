63311573e6f62747190f35739589f7a3c9fb6b53
meitetsu5000 <nogizakapython@outlook.jp>
2024-09-24 07:11:58 +0900

    Modefy20240924

---
+++ paiza_dp2.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+x, d = map(int, input().split())
+
+a = [x] * (1000 + 1)
+
+for i in range(2, 1000 + 1):
+    a[i] = a[i - 1] + d
+
+q = int(input())
+
+for i in range(q):
+    k = int(input())
+    print(a[k])
+
