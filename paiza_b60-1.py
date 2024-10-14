b57815216a3c321b15a3fad9c54613632ffa4f59
meitetsu5000 <nogizakapython@outlook.jp>
2024-10-15 06:16:57 +0900

    Modefy20241015

---
+++ paiza_b60-1.py
@@ -0,0 +1,10 @@
+user2blood = {
+    "Kyoko": "B",
+    "Rio": "O",
+    "Tsubame": "AB",
+    "KurodaSensei": "A",
+    "NekoSensei": "A",
+}
+
+for user, blood in user2blood.items():
+    print(user + " " + blood)
