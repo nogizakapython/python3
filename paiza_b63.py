7e339273fad0f5606acf63932b7866234081a31b
meitetsu5000 <nogizakapython@outlook.jp>
2024-10-20 19:31:40 +0900

    Modefy20241020

--- paiza_b63.py
+++ paiza_b63.py
@@ -1,16 +1,11 @@
-using System;
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+t_blood = input()
+n = int(input())
+dict1 = {}
+for i in range(n):
+    key,value = input().split(' ')
+    dict1[key] = value
 
-class Program
-{
-    static void Main()
-    {
-        // 自分の得意な言語で
-        // Let's チャレンジ！！
-        int n = Int32.Parse(Console.ReadLine());
-        var line = Console.ReadLine();
-        string[] array1 = line.Split(',');
-        foreach(string s in array1){
-            Console.WriteLine(s);
-        }
-    }
-}
+print(dict1[t_blood])
