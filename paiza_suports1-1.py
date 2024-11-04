138c78a5f7ebdc17420e1f66146ca43a36526ab8
meitetsu5000 <nogizakapython@outlook.jp>
2024-11-04 18:24:35 +0900

    Modefy20241104

---
+++ paiza_suports1-1.py
@@ -0,0 +1,11 @@
+s = input()
+if s == 'baseball':
+    print(9)
+elif s == 'soccer':
+    print(11)
+elif s == 'rugby':
+    print(15)
+elif s == 'basketball':
+    print(5)
+elif s == 'volleyball':
+    print(6)
