39089683bfef61cfbd23afe54f68a3d3c559e94e
meitetsu5000 <nogizakapython@outlook.jp>
2024-11-04 18:23:43 +0900

    Modefy20241104

---
+++ paiza_suports1.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+s = input()
+dict1 = {
+            "baseball": 9,
+            "soccer" : 11,
+            "rugby" : 15,
+            "basketball" : 5,
+            "volleyball" : 6
+    }
+print(dict1[s])
