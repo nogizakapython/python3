c93a82bd4ff3657945aa6bf3fb2bc26261ad0de2
meitetsu5000 <nogizakapython@outlook.jp>
2024-11-30 15:47:24 +0900

    Modefy20241130

---
+++ paiza_d317.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+array1 = []
+for i in range(2):
+    word = input()
+    array1.append(word)
+
+if array1[0] == array1[1]:
+    print("Yes")
+else:
+    print("No")
