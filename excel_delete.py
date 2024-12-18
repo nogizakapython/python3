2cee1cc7f191323c394a57db32aae8d283ec6e22
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-12-18 11:08:09 +0900

    Modefy20241218

--- excel_delete.py
+++ excel_delete.py
@@ -1,16 +1,21 @@
 # 行削除テスト
 
-
+# ライブラリのインポート
 import openpyxl
 
+# Global変数
+# ファイル名
 file_name = "data1.xlsx"
-
+# ExcelのWorkbookインスタンスの呼び出し
 wb = openpyxl.load_workbook(file_name)
+# ExcelのWorksheet変数を定義する
 ws = wb['Sheet1']
 
-
+# 行の最終行を取得する関数
 def end_row():
+    # 開始行変数
     start_num = 4
+    # カウント変数
     i = start_num
     while True:
         dte_str = ws.cell(i,4).value
@@ -21,4 +26,19 @@ def end_row():
     return i-1        
 
 finish_row_num = end_row()
-print(finish_row_num)
+
+count = 4
+while True:
+    div = ws.cell(count,4).value
+    # 部署が「Tecnplogy」の時、１行削除する
+    if div == "Tecnology":
+        # print("削除するよ～")
+        # print(count)
+        ws.delete_rows(count)
+    elif div == None:
+        break    
+    else:    
+        count += 1
+
+# 削除処理後、ファイルを保存する
+wb.save(file_name)    
