0e6323bc6cf1e9245a823a9ef590e008a3d663d1
meitetsu5000 <nogizakapython@outlook.jp>
2025-09-18 06:15:17 +0900

    Modefy20250918

---
+++ paiza_class1-1.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for _ in range(N):
+    n, o, b, s = input().split()
+
+    print("User{")
+    print("nickname : " + n)
+    print("old : " + o)
+    print("birth : " + b)
+    print("state : " + s)
+    print("}")
