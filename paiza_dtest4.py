3b1be69a980d5772c33cb1831ede55cf463be3d9
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-03-11 10:51:44 +0900

    Modefy20260310

--- paiza_dtest4.py
+++ paiza_dtest4.py
@@ -1,6 +1,25 @@
 # coding: utf-8
 # 自分の得意な言語で
 # Let's チャレンジ！！
-num = int(input())
-str1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-print(str1[num-1])
+array1 = input().split(" ")
+n = int(array1[0])
+m = int(array1[1])
+data_array = []
+for i in range(n):
+    data_array.append(0)
+
+for j in range(m):
+    w_array = input().split(' ')
+    a = int(w_array[0])
+    b = int(w_array[1])
+    data_array[a-1] = b
+    w_count = 0
+    elem = len(data_array)
+    for k in range(elem):
+        data = data_array[k]
+        if data == 0:
+            w_count += 1 
+            
+    if w_count == 0:
+        break
+print(sum(data_array))
