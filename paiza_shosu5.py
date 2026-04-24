20992168916e54b533d72cb465b7ad84d31d57b5
meitetsu5000 <nogizakapython@outlook.jp>
2026-04-25 01:19:56 +0900

    Modefy20260425

---
+++ paiza_shosu5.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# ==========================================================
+# 【Python3】標準入力の書き方に困ったらこちら！
+#
+# 「入力される値」の取得方法一覧（Python）
+# https://paiza.jp/pages/works/cheatsheet/stdin_python
+#
+# 練習用問題セット（レベルアップ問題集）
+# https://paiza.jp/works/mondai/cd_rank_input_problems/problem_index?language_uid=python3
+# ==========================================================
+# ここからコードを書き始めてください
+n = float(input())
+print(n + 0.1)
