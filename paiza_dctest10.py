7ac77ec80ad9ed1fce092a47a337f532e062f499
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-06-01 11:03:25 +0900

    Modefy20260601

---
+++ paiza_dctest10.py
@@ -0,0 +1,14 @@
+# ==========================================================
+# 【Python3】標準入力の書き方に困ったらこちら！
+# 
+# 「入力される値」の取得方法一覧（Python）
+# https://paiza.jp/pages/works/cheatsheet/stdin_python
+# ==========================================================
+# ここからコードを書き始めてください
+a,b,c,d = map(int,input().split(' '))
+s = input()
+print(a + 1)
+print(b + 1)
+print(c + 1)
+print(d + 1)
+print(s)
