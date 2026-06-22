e9b72d6ef960bfdece64aa9a002fb7a158d391c4
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-06-22 12:00:18 +0900

    Modedy20260622

---
+++ paiza_seiki14.py
@@ -0,0 +1,12 @@
+# ==========================================================
+# 【Python3】標準入力の書き方に困ったらこちら！
+# 
+# 「入力される値」の取得方法一覧（Python）
+# https://paiza.jp/pages/works/cheatsheet/stdin_python
+# ==========================================================
+# ここからコードを書き始めてください
+import re
+
+str1 = input()
+ans = (re.search(r'accept|reject|pending',str1).start())
+print(ans)
