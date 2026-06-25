a26419e6e85319ae8ca7dc6e343d4384b8e4289e
meitetsu5000 <nogizakapython@outlook.jp>
2026-06-26 07:04:49 +0900

    MNodefy20260626

---
+++ paiza_d390.py
@@ -0,0 +1,15 @@
+# ==========================================================
+# 【Python3】標準入力の書き方に困ったらこちら！
+#
+# 「入力される値」の取得方法一覧（Python）
+# https://paiza.jp/pages/works/cheatsheet/stdin_python
+# ==========================================================
+# ここからコードを書き始めてください
+A = int(input())
+B = int(input())
+if A > B:
+    print("A")
+elif A < B:
+    print("B")
+else:
+    print("DRAW")
