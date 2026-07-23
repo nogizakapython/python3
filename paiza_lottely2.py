1f8f584ec70794b2b4803c53a721c1234d3291cc
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-07-23 13:16:13 +0900

    Modefy20260723

---
+++ paiza_lottely2.py
@@ -0,0 +1,14 @@
+# ==========================================================
+# 【Python3】標準入力の書き方に困ったらこちら！
+# 
+# 「入力される値」の取得方法一覧（Python）
+# https://paiza.jp/pages/works/cheatsheet/stdin_python
+# ==========================================================
+# ここからコードを書き始めてください
+a = int(input())
+print(a)
+n = int(input())
+print(n)
+for i in range(n):
+    d = int(input())
+    print(d)
