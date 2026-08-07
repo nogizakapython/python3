8b6ccd96ecd840f2ba5379f6e461e215b7eed288
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-08-07 15:47:25 +0900

    Modefy20260807

---
+++ paiza_seiki21.py
@@ -0,0 +1,14 @@
+# ==========================================================
+# 【Python3】標準入力の書き方に困ったらこちら！
+# 
+# 「入力される値」の取得方法一覧（Python）
+# https://paiza.jp/pages/works/cheatsheet/stdin_python
+# ==========================================================
+# ここからコードを書き始めてください
+import re
+
+
+s = input()
+array1 = re.findall(r'[^ ]+@[^ ]+',s)
+for data in array1:
+    print(data)
