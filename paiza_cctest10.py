f8ad54c1f1b8ff0424a5c18dda8d5252e07a17dc
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-08-19 13:16:05 +0900

    Modefy20260819

---
+++ paiza_cctest10.py
@@ -0,0 +1,10 @@
+# ==========================================================
+# 【Python3】標準入力の書き方に困ったらこちら！
+# 
+# 「入力される値」の取得方法一覧（Python）
+# https://paiza.jp/pages/works/cheatsheet/stdin_python
+# ==========================================================
+# ここからコードを書き始めてください
+array1 = list(map(str,input().split(' ')))
+for s in list(array1):
+    print(s)
