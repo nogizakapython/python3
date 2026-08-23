278108b2a48221ebf69618cd4b87146ce379ad94
meitetsu5000 <nogizakapython@outlook.jp>
2026-08-23 21:06:08 +0900

    Modefy20260823

---
+++ paiza_d398.py
@@ -0,0 +1,13 @@
+# ==========================================================
+# 【Python3】標準入力の書き方に困ったらこちら！
+#
+# 「入力される値」の取得方法一覧（Python）
+# https://paiza.jp/pages/works/cheatsheet/stdin_python
+# ==========================================================
+# ここからコードを書き始めてください
+array1 = list(input())
+count = 0
+for i in list(array1):
+    if i == "8":
+        count += 1
+print(count)
