f4f5623de97dcd74fdd6c67335e0961c942b410b
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-08-24 10:56:16 +0900

    Modefy20260824

---
+++ paiza_template12.py
@@ -0,0 +1,11 @@
+# ==========================================================
+# 【Python3】標準入力の書き方に困ったらこちら！
+# 
+# 「入力される値」の取得方法一覧（Python）
+# https://paiza.jp/pages/works/cheatsheet/stdin_python
+# ==========================================================
+# ここからコードを書き始めてください
+array1 = list(map(int,input().split(" ")))
+for i in array1:
+    ans = i + 1
+    print(ans)
