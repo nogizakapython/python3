ad95f61ca2eadda1b4de60b0cd04306579f7f8a9
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-08-31 11:07:07 +0900

    Modefy20260831

--- paiza_seiki24.py
+++ paiza_seiki24.py
@@ -1,13 +1,14 @@
 # ==========================================================
 # 【Python3】標準入力の書き方に困ったらこちら！
-#
+# 
 # 「入力される値」の取得方法一覧（Python）
 # https://paiza.jp/pages/works/cheatsheet/stdin_python
 # ==========================================================
 # ここからコードを書き始めてください
-
 import re
 
 s = input()
-ans = re.sub(r'/\*.*\*/',r'',s,1)
-print(ans)
+s_1 = re.sub(r'\-{3,}',"/",s)
+array1 = s_1.split("/")
+for str1 in array1:
+    print(str1)
