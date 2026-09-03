45323308636e58fb96cbd056c397b95beeeabc33
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-09-03 11:09:34 +0900

    Modefy20260903

---
+++ paiza_senkei17-1.py
@@ -0,0 +1,13 @@
+n = int(input())
+a = [int(x) for x in input().split()]
+k = int(input())
+
+maximum = 1000000001
+for i in range(k):
+    next_maximum = -1000000001
+    for val in a:
+        if val < maximum:
+            next_maximum = max(next_maximum, val)
+    maximum = next_maximum
+
+print(maximum)
