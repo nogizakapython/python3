7c4a70a850e5d83df75fb79da622b29aabfba4df
meitetsu5000 <nogizakapython@outlook.jp>
2026-09-09 12:57:00 +0900

    Modefy20260909

---
+++ paiza_senkei14.py
@@ -0,0 +1,13 @@
+# ==========================================================
+# 【Python3】標準入力の書き方に困ったらこちら！
+#
+# 「入力される値」の取得方法一覧（Python）
+# https://paiza.jp/pages/works/cheatsheet/stdin_python
+# ==========================================================
+# ここからコードを書き始めてください
+n = int(input())
+array1 = [ input().split() for _ in range(n)]
+k,l = map(int,input().split(' '))
+for name,score in array1:
+    if int(score) >= k and int(score) <= l:
+        print(name)
