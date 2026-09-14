049546e3a68a8b39db2897252edaad93c2daf05c
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-09-14 11:42:53 +0900

    Modefy20260914

---
+++ paiza_stack6-1.py
@@ -0,0 +1,20 @@
+import queue
+
+Q = int(input())
+
+q = queue.Queue()  # キュー本体
+for i in range(Q):
+    query = input().split()
+    if query[0] == "1":
+        # PUSH
+        q.put(query[1])
+    elif query[0] == "2":
+        print(q.get())
+    for j in range(q.qsize()):  # キューの要素分だけループを回す
+        tmp = q.get()
+        if j < q.qsize():
+            print(tmp, end=" ")
+        else:
+            print(tmp, end="")
+        q.put(tmp)
+    print()
